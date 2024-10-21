c5d4253800ab3060150f26a609480fa938000cd1
Bxxst_ <pawel.scib2@gmail.com>
2024-10-21 20:32:03 +0200

    description update

--- src/pages/mainpage.js
+++ src/pages/mainpage.js
@@ -7,13 +7,13 @@ import BackgroundVideo from "./backgroundvideo";
 import Item from "./itemdiv";
 
 export const items = [
-  {id: 1, name: "T-SHIRT MONOCHRXME COLLECTION", price: 100, image: require("../images/tshirts/monochrome.png"), description: "dsa"},
-  {id: 2, name: "T-SHIRT LOST SOUL COLLECTION", price: 100, image: require("../images/tshirts/lostsoul.png"), description: "dsad"},
-  {id: 3, name: "T-SHIRT PAINKILLER COLLECTION", price: 100, image: require("../images/tshirts/painkiller.png"), description: "dsada"},
-  {id: 4, name: "T-SHIRT THORNS COLLECTION", price: 150, image: require("../images/tshirts/thorns.png"), description: "dsada"},
-  {id: 5, name: "T-SHIRT TOKIO COLLECTION", price: 100, image: require("../images/tshirts/tokio.png"), description: "dsada"},
-  {id: 6, name: "T-SHIRT SNOWBUNNY COLLECTION", price: 200, image: require("../images/tshirts/snowbunny.png"), description: "dsada"},
-  {id: 7, name: "T-SHIRT INCEPTION COLLECTION", price: 150, image: require("../images/tshirts/mug.png"), description: "dsada"}
+  {id: 1, name: "T-SHIRT MONOCHRXME COLLECTION", price: 100, image: require("../images/tshirts/monochrome.png"), description: "Czarno-biała bluzka T-shirt o luźnym kroju. Materiał 100% bawełna, wzór oraz krój oryginalny, zaprojektowany przez naszą wytwórnie."},
+  {id: 2, name: "T-SHIRT LOST SOUL COLLECTION", price: 100, image: require("../images/tshirts/lostsoul.png"), description: "Czarno-biała bluzka T-shirt o luźnym kroju. Materiał 100% bawełna, wzór oraz krój oryginalny, zaprojektowany przez naszą wytwórnie."},
+  {id: 3, name: "T-SHIRT PAINKILLER COLLECTION", price: 100, image: require("../images/tshirts/painkiller.png"), description: "Czarno-biała bluzka T-shirt o luźnym kroju. Materiał 100% bawełna, wzór oraz krój oryginalny, zaprojektowany przez naszą wytwórnie."},
+  {id: 4, name: "T-SHIRT THORNS COLLECTION", price: 150, image: require("../images/tshirts/thorns.png"), description: "Czarno-biała bluzka T-shirt o luźnym kroju. Materiał 100% bawełna, wzór oraz krój oryginalny, zaprojektowany przez naszą wytwórnie."},
+  {id: 5, name: "T-SHIRT TOKIO COLLECTION", price: 100, image: require("../images/tshirts/tokio.png"), description: "Czarno-biała bluzka T-shirt o luźnym kroju. Materiał 100% bawełna, wzór oraz krój oryginalny, zaprojektowany przez naszą wytwórnie."},
+  {id: 6, name: "T-SHIRT SNOWBUNNY COLLECTION", price: 200, image: require("../images/tshirts/snowbunny.png"), description: "Czarno-biała bluzka T-shirt o luźnym kroju. Materiał 100% bawełna, wzór oraz krój oryginalny, zaprojektowany przez naszą wytwórnie."},
+  {id: 7, name: "T-SHIRT INCEPTION COLLECTION", price: 150, image: require("../images/tshirts/mug.png"), description: "Czarno-biała bluzka T-shirt o luźnym kroju. Materiał 100% bawełna, wzór oraz krój oryginalny, zaprojektowany przez naszą wytwórnie."}
 ]
 
 function Mainpage(){
